bf5aaa5e78f2ad348d84075afff64807f2c2a7b2
tohirbek009 <odilovtohirbek009@gmail.com>
2021-10-31 13:40:44 +0500

    Feed component changed

--- src/reducer.js
+++ src/reducer.js
@@ -13,9 +13,6 @@ const reducer = (state, action) => {
     console.log("user:",action)
     switch(action.type){
         case actionTypes.SET_USER:
-            db.collection('users').add({
-                userInfo: action.user.displayName
-            })
             return {...state, user: action.user,}
         default: return state;
     }
